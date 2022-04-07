1f3ebeff5f57d438b24a0c306057c8b3db387d94
vardhan6065 <vardhandaphalea@gmail.com>
2022-04-08 00:40:34 +0530

    updated

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,89 @@
+import classes from './Checkout.module.css';
+import { useRef , useState} from 'react';
+
+const isEmpty = value=>value.trim() === '';
+const isNotFiveChars = value => value.trim().length!== 5;
+
+
+const Checkout = (props) => {
+
+  const [formInputsValidity,setFormInputsValidity ]= useState({
+    name : true,
+    street:true,
+    city:true,
+    postalCode:true 
+  })
+
+  const nameInputRef = useRef();  
+  const streetInputRef = useRef();  
+  const postalCodeInputRef = useRef();  
+  const cityInputRef = useRef();  
+
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value; 
+    const enteredStreet = streetInputRef.current.value; 
+    const enteredPostalCode = postalCodeInputRef.current.value; 
+    const enteredCity = cityInputRef.current.value; 
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalCodeIsValid = !isNotFiveChars(enteredPostalCode);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+
+    setFormInputsValidity({
+      name : enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid= enteredCityIsValid && enteredNameIsValid && enteredPostalCodeIsValid && enteredStreetIsValid;
+
+    if(!formIsValid){
+        return 
+    }
+    
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode,
+    });
+};
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={`${classes.control} ${formInputsValidity.name ? '' : classes.invalid}`}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please ENter A valid name!!</p> }
+      </div>
+      <div className={`${classes.control} ${formInputsValidity.street ? '' : classes.invalid}`}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' ref={streetInputRef}/>
+        {!formInputsValidity.street && <p>Street shoukd not be empty!!</p> }
+      </div>
+      <div className={`${classes.control} ${formInputsValidity.postalCode ? '' : classes.invalid}`}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && <p>Please Enter A valid Postal Code!!</p> }
+      </div>
+      <div className={`${classes.control} ${formInputsValidity.city  ? '' : classes.invalid}`}>
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please fill this field!!</p> }
+      </div>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
